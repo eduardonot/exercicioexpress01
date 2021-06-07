aecc8ebc14d883df7fbc5b1c1b19457967e46ea7
eduardonot <eduardo_not@hotmail.com>
2021-06-07 10:43:34 -0300

    test

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,8 +1,5 @@
-const express = require ('express')
-const router = express.Router()
-
 module.exports = router => {
     router.get('/', function (req, res){
-        res.send('Bem-Vindo!')
+        res.send('Home')
     })
 }
