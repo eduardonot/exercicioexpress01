273213c9b7cb38a8a8c2c519c8b2efc0eb2b242e
eduardonot <eduardo_not@hotmail.com>
2021-09-15 08:48:17 -0300

    start refact session

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
+const session = require('express-session')
 const db = require('./infra/mongodb')
 const app = express()
 const telegramRoute = require('./routes/telegram-route')
-const session = require('express-session')
 const config = require('./config')
 
 app.use(session({
